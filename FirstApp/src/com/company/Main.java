510adb3cfd3cc07d7f3659eadbf0e6c3cd035da4
amirholyanasabnarab <amirholyanasabnarab@gmail.com>
2021-05-14 18:20:54 +0430

    Session-01

---
+++ FirstApp/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String message = "Hello Java";
+        System.out.println(message);
+    }
+}
