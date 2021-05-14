f6098639ad9c8b4ec3f6312ca0d96cdebed34554
amirholyanasabnarab <amirholyanasabnarab@gmail.com>
2021-05-14 19:13:38 +0430

    FirstApp

--- FirstApp/src/com/company/Person.java
+++ FirstApp/src/com/company/Person.java
@@ -5,6 +5,12 @@ public class Person {
     String firstName;
     String lastName;
 
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
     public void sayHello(){
         System.out.printf("Welcome %s %s to Java world",this.firstName,this.lastName);
     }
