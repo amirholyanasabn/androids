e9a0df41f99c0d3682489691094d2e518d38fb1d
amirholyanasabnarab <amirholyanasabnarab@gmail.com>
2021-05-16 13:17:35 +0430

    FinalKeywordDemo

---
+++ FinalDemo/src/com/company/Person.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public   class Person {
+    final int myVar=20 ;
+
+    public final void sayHello(){
+        System.out.println("Hello");
+    }
+}
---
+++ FinalDemo/src/com/company/Student.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public  class Student extends Person {
+//    @Override
+//    public void sayHello() {
+//        super.sayHello();
+//    }
+}
